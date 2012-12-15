3f637584d4ee6305ac918a82d6967e425f30e4b9
wanghj <wanghj@sina.com>
2012-12-16 03:04:45 +0800

    parkingManager初建

---
+++ src/edu/buaa/finalexam/ParkingManager.java
@@ -0,0 +1,11 @@
+package edu.buaa.finalexam;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wanghj
+ * Date: 12-12-16
+ * Time: 上午3:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParkingManager {
+}
